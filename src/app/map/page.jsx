aa2e8b579ee8199c175f3fe97dec0bb81b18a575
Dev Shakya <devxoshakya@gmail.com>
2024-08-25 13:03:21 +0530

    added db connection

--- src/app/map/page.jsx
+++ src/app/map/page.jsx
@@ -1,7 +1,26 @@
+"use client";
 import ClientSideMap from './ClientSideMap';
+import { useEffect, useState } from "react";
 
 const YourPage = () => {
+
+  const [geofences, setGeofences] = useState([]);
+
+  useEffect(() => {
+    const fetchGeofences = async () => {
+      const response = await fetch("/api/getGeofences");
+      const data = await response.json();
+      setGeofences(data);
+    };
+
+    fetchGeofences();
+  }, []);
+
+  console.log(geofences);
+
+
   return (
+    
     <div>
       <ClientSideMap />
     </div>
